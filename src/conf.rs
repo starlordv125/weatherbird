f9f9b6572bfc8ec3c7701586fe5a6a9c59cd1016
Cameron Reynolds <starlordv125@keemail.me>
2026-06-18 13:39:20 -0400

    start to creating file

--- src/conf.rs
+++ src/conf.rs
@@ -5,6 +5,11 @@ const CONF: &str = "~/.config/duck/duck.conf";
 pub fn write_conf() {
     match fs::metadata(CONF) {
         Ok(_) => {println!("Exists!")}
-        Err(_) => {println!("Does not exist")}
+        Err(_) => {
+            println!("Creating configuration file in {}", CONF);
+            let path = std::path::Path::new(CONF);
+            let prefix = path.parent().expect("Fatal error in write_conf()");
+            std::fs::create_dir_all(prefix).expect("Error creating directory");
+        }
     }
 }
