8368d0817d38bc13261a76302d3034643e9bc364
Cameron Reynolds <starlordv125@keemail.me>
2026-07-28 23:46:12 -0400

    Workflow test *8

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # This builds rust binaries and prepares them for release
-cargo install cross --git https://github.com/cross-rs/cross
-cargo build --release
-cross build --release --target=aarch64-unknown-linux-gnu
+echo works
