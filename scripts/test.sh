65d94918ca940e7b294c62702706d7eba07baccc
Cameron Reynolds <starlordv125@keemail.me>
2026-07-29 15:27:10 -0400

    Workflow test *12

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,11 @@
 
 # This builds rust binaries and prepares them for release
 echo works
+echo Installing cross
 cargo install cross --git https://github.com/cross-rs/cross
+echo Adding aarch64 target
 rustup target add aarch64-unknown-linux-gnu
-cargo build --release
-cross build --release --target=aarch-unknown-linux-gnu
+#cargo build --release
+pwd
+echo Starting cross
+bash cross build --release --target=aarch-unknown-linux-gnu
