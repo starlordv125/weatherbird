38677a73aab4f60b46d61392fc9ba8faab8936f7
Cameron Reynolds <starlordv125@keemail.me>
2026-08-13 10:03:13 -0400

    CI test *11

--- scripts/rpm.sh
+++ scripts/rpm.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Sign RPM
-rpmsign -k ./decoded-key.gpg -p $GPG_PASS -n "Cameron Reynolds <cameron@starlordv125.net>" -s "target/generate-rpm/weatherbird-$VERSION-2.x86_64.rpm" -f "_##" -o signed_rpm/
-
 # Upload RPM
 curl --user starlordv125:$API_KEY \
---upload-file signed_rpm/weatherbird-$VERSION-2.x86_64.rpm \
+--upload-file target/generate-rpm/weatherbird-$VERSION-3.x86_64.rpm \
 https://forgejo.starlordv125.net/api/packages/starlordv125/rpm/upload
