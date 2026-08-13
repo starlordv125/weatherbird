eec4afa7604b36d0dc3794545570db112b26e85f
Cameron Reynolds <starlordv125@keemail.me>
2026-08-13 10:20:21 -0400

    Revert test changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 cargo build --release
-cargo generate-rpm
--- scripts/rpm.sh
+++ scripts/rpm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Build RPM
+cargo generate-rpm
+
 # Upload RPM
 curl --user starlordv125:$API_KEY \
---upload-file target/generate-rpm/weatherbird-$VERSION-3.x86_64.rpm \
+--upload-file target/generate-rpm/weatherbird-$VERSION-1.x86_64.rpm \
 https://forgejo.starlordv125.net/api/packages/starlordv125/rpm/upload
